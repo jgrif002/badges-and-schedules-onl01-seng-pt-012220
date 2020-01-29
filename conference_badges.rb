9ad8abd2f0e18bc3bfea0502c23c0ba6ae5df043
Jesse Griffin <jesse.griffin@gmail.com>
2020-01-29 05:06:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def assign_rooms(speakers)
   speakers.each_with_index do|name, i|
   new_arr << "Hello, #{name}! You'll be assigned to room #{i+1}!"
   end 
-  new_arr
+  return new_arr
 end
 
 def printer(attendees)
